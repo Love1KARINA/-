011eb92bce4217a1dcf61baf8572d89780af4ce8
Love1KARINA <1648241930@qq.com>
2023-08-19 10:44:06 +0800

    first commit

--- user/sleep.c
+++ user/sleep.c
@@ -1,12 +1,13 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
-{
-if(argc !=2){
-fprintf(2, "Error: Parameters Error for Sleep\n");
-exit(1);
-}
-sleep(atoi(argv[1])); exit(0);
+int main(int argn, char *argv[]){
+    if(argn != 2){
+        fprintf(2, "must 1 argument for sleep\n");
+        exit(1);
+    }
+    int sleepNum = atoi(argv[1]);
+    printf("(nothing happens for a little while)\n");
+    sleep(sleepNum);
+    exit(0);
 }
